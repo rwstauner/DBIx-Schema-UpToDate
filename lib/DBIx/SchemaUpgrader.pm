fbca26d739122457bb605fbe0ef07714e3c58593
Randy Stauner <randy@magnificent-tears.com>
2011-02-28 14:28:26 -0700

    Break up logic into more methods to ease testing
    and subclassing

--- lib/DBIx/SchemaUpgrader.pm
+++ lib/DBIx/SchemaUpgrader.pm
@@ -17,11 +17,16 @@ Current option:
 sub new {
 	my $class = shift;
 	my $self = {
+		build => 1,
 		@_ == 1 ? %{$_[0]} : @_
 	};
 	bless $self, $class;
-	$self->build;
-	$self;
+
+	# make sure the database schema is current
+	$self->build()
+		if $self->{build};
+
+	return $self;
 }
 
 =item dbh
@@ -37,7 +42,7 @@ sub dbh {
 
 =item build
 
-Builds the database from its current version to the latest version
+Builds the database from L</current_version> to L</latest_version>
 performing whatever tasks may be necessary to bring the schema up to date.
 
 =cut
@@ -46,24 +51,36 @@ sub build {
 	my ($self) = @_;
 	my $dbh = $self->dbh;
 
+	my $current = $self->current_version;
+	my $latest  = $self->latest_version;
+
+	# execute each instruction required to go from current to latest version
+	# (starting with next version, obviously (don't redo current))
+	$self->update_to_version($_)
+		foreach ($current + 1) .. $latest;
+}
+
+=item current_version
+
+Determine the current version of the database schema.
+
+=cut
+
+sub current_version {
+	my ($self) = @_;
+	my $dbh = $self->dbh;
 	my $version = -1;
 	my $tables = $dbh->table_info()->fetchall_hashref('table_name');
 
 	if( exists($tables->{version}) ){
-		$version = $dbh->selectcol_arrayref(
+		my $v = $dbh->selectcol_arrayref(
 			'SELECT version from version'
 		)->[0];
+		$version = $v
+			if defined $v;
 	}
 
-	my $changes = $self->instructions();
-	for(my $v = $version + 1; $v < @$changes; ++$v){
-		$dbh->begin_work();
-		# execute instructions to update database to version $v
-		$changes->[$v]->($self);
-		# save the version now in case we get interrupted before the next save
-		$dbh->do('UPDATE version SET version = ?', {}, $v);
-		$dbh->commit();
-	}
+	return $version;
 }
 
 =item instructions
@@ -77,13 +94,62 @@ on the L</dbh> until the database schema is up to date.
 
 sub instructions {
 	my ($self) = @_;
-	return [
+	return $self->{instructions} ||= [
 		# v0: 2011-02-08: database version metadata
 		sub {
 			$self->dbh->do('CREATE TABLE version (version integer)');
 			$self->dbh->do('INSERT INTO  version (version) VALUES(0)');
 		},
 	];
+}
+
+=item latest_version
+
+Returns the latest [possible] version of the database schema.
+
+=cut
+
+sub latest_version {
+	my ($self) = @_;
+	return $#{ $self->instructions };
+}
+
+=item set_version
+
+	$cache->set_version($verison);
+
+Sets the current database version to C<$version>.
+Called from L</update_to_version> after executing the appropriate instruction.
+
+=cut
+
+sub set_version {
+	my ($self, $version) = @_;
+	$self->dbh->do('UPDATE version SET version = ?', {}, $version);
+}
+
+=item update_to_version
+
+	$cache->update_to_version($version);
+
+Executes the instruction associated with C<$version>
+in order to bring database up to that version.
+
+=cut
+
+sub update_to_version {
+	my ($self, $version) = @_;
+	my $dbh = $self->dbh;
+
+	$dbh->begin_work();
+
+	# execute instructions to update database to $version
+	$self->instructions->[$version]->($self);
+
+	# save the version now in case we get interrupted before the next commit
+	$self->set_version($version);
+
+	$dbh->commit();
 }
 
 1;
@@ -110,6 +176,8 @@ sub instructions {
 	# do something with $dbh which now contains the schema you expect
 
 =head1 DESCRIPTION
+
+B<NOTE>: This API is under construction and subject to change.
 
 =head1 TODO
 
