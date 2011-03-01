df189e9d68d6256ae409bd23ed73bbd8b4c5ef49
Randy Stauner <randy@magnificent-tears.com>
2011-02-28 18:01:57 -0700

    Expound on documentation
    * Description
    * Rationale
    * See Also

--- lib/DBIx/SchemaUpgrader.pm
+++ lib/DBIx/SchemaUpgrader.pm
@@ -7,10 +7,16 @@ use warnings;
 =method new
 
 Constructor;  Accepts a hash or hashref of options.
-Current option:
+
+Options used by the base module:
 
 =for :list
 * C<dbh> - A B<d>ataB<b>ase B<h>andle (as returned from C<< DBI->connect >>)
+Database commands will be executed against this handle.
+* C<build> - Boolean
+By default L</build> is called at initialization (after being blessed).
+Set this value to false to disable this if you need to do something else
+before building.  You will have to call L</build> yourself.
 
 =cut
 
@@ -205,25 +211,64 @@ sub version_table_name {
 	}
 
 	package main;
-	my $dbh = Local::Database->new()->dbh;
+
+	my $dbh = DBI->connect(@connection_args);
+	Local::Database->new(dbh => $dbh);
 
 	# do something with $dbh which now contains the schema you expect
 
 =head1 DESCRIPTION
 
-B<NOTE>: This API is under construction and subject to change.
+This module provides a base class for keeping a database schema up to date.
+If you need to make changes to the schema
+in remote databases in an automated manner
+you may not be able to ensure what version of the database is installed
+by the time it gets the update.
+This module will apply patches sequentially to bring the database schema
+up to the latest version from whatever the current version is.
+
+The aim of this module is to enable you to write incredibly simple subclasses
+so that all you have to do is define the updates you want to apply.
+This is done with subs (coderefs) so you can access the object
+and its database handle.
+
+It is intentially simple and is not intended for large scale applications.
+Check L</SEE ALSO> for alternative solutions
+and pick the one that's right for your situation.
+
+=head1 USAGE
+
+Subclasses should overwrite L</instructions>
+to return an arrayref of subs (coderefs) that will be executed
+to bring the schema up to date.
+
+The rest of the methods are small in the hopes that you
+can overwrite the ones you need to get the customization you require.
 
 =head1 TODO
 
 =for :list
 * Rename this module
-* use L<DBI/quote_identifier> on the table name
+* Use L<DBI/quote_identifier> on the table name
+
+=head1 RATIONALE
+
+I had already written most of the logic for this module in another project
+when I realized I should abstract it.
+Then I went looking and found the modules listed in L</SEE ALSO>
+but didn't find one that fit my needs, so I released what I had made.
 
 =head1 SEE ALSO
 
 =for :list
 * L<DBIx::VersionedSchema>
+Was close to what I was looking for, but not customizable enough.
+Were I to subclass it I would have needed to overwrite the two main methods.
 * L<DBIx::VersionedDDL>
+Much bigger scale than what I was looking for.
+Needed something without Moose.
 * L<ORLite::Migrate> (L<http://use.perl.org/~Alias/journal/38087>)
+Much bigger scale than what I was looking for.
+Wasn't using ORLite, and didn't want to use separate script files
 
 =cut
